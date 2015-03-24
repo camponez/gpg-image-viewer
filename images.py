0ecc654bbef4bcd57b56e941aa59383219f19a91
Eduardo Elias <camponez@gmail.com>
2015-03-23 23:00:16 -0300

    Add Makefile

--- images.py
+++ images.py
@@ -11,8 +11,8 @@ This file is part of GPG Image Viewer.
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License.  If not,
+    see <http://www.gnu.org/licenses/>.
 """
 
 import os
